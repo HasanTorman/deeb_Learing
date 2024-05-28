4abb4fa5fb4d3fa07dba21c57ff52008e2328a35
HasanTorman <torman.hassan@gmail.com>
2024-05-28 17:15:48 +0300

    feat :git rm -r --cached

--- database/index.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('testdb', 'postgres', '0000', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
-
-module.exports = sequelize;
--- utils/timestamp.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-
-const getCurrentTimestamp = () => {
-  return moment().toDate();
-};
-
-module.exports = {
-  getCurrentTimestamp
-};
